50f1a345344020a0a4310f8b29fce62b81839ceb
Mark Wadham <m@rkw.io>
2017-10-20 10:41:25 +0100

    update

--- icloudControl/main.swift
+++ icloudControl/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+if CommandLine.arguments.count < 3 {
+    print("Usage: \(CommandLine.arguments[0]) [offload|restore] <path>")
+    exit(0)
+}
 
+let fm = FileManager.default
+
+let url = URL.init(fileURLWithPath: CommandLine.arguments[2], isDirectory: false)
+
+if CommandLine.arguments[1] == "offload" {
+  do {
+    try fm.evictUbiquitousItem(at: url)
+    print("file offloaded.")
+  } catch {
+    print("failed: \(error)")
+  }
+}
+
+if CommandLine.arguments[1] == "restore" {
+    do {
+        try fm.startDownloadingUbiquitousItem(at: url)
+        print("file restored.")
+    } catch {
+        print("failed: \(error)")
+    }
+}
